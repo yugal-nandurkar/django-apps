b2825ca59c22ae1c38550ee527b0a9d811d6a7e6
yugal-nandurkar <yugalnandurkar5@gmail.com>
2024-10-24 11:46:36 +0530

    Polling Web App Made in Django

---
+++ polling_web_app/polls/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        #
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+
+    ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+#admin.site.register(Choice)
+# Register your models here.
---
+++ polling_web_app/polls/models.py
@@ -0,0 +1,36 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    list_filter = ["pub_date"] #find where exactly to add this line
+    search_fields = ["question_text"] #this line as well
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+       # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+# Create your models here.
